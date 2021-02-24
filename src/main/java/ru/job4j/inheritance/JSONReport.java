f9991de0e04e9ce539595c348101914c83354d46
John_Smith <the.tylertyler@gmail.com>
2021-02-25 00:05:26 +0300

    5. Аннотация @Override [#459532]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport{
+    @Override
     public String generate(String name, String body) {
         char quote = '"';
         return "{"+ System.lineSeparator() + "  " + quote + "name" + quote + " : " + quote + name + quote + "," + System.lineSeparator() + "  " + quote + "body" + quote + " : " + quote + body + quote + System.lineSeparator() + "}";
