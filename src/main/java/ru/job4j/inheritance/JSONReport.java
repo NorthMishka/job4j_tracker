cdea2302f5a38a2bf9858f97940291ada65f77b7
John_Smith <the.tylertyler@gmail.com>
2021-02-24 23:50:46 +0300

    4. Переопределение [#459531]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport{
+    public String generate(String name, String body) {
+        char quote = '"';
+        return "{"+ System.lineSeparator() + "  " + quote + "name" + quote + " : " + quote + name + quote + "," + System.lineSeparator() + "  " + quote + "body" + quote + " : " + quote + body + quote + System.lineSeparator() + "}";
+    }
+}
+
--- src/main/java/ru/job4j/profession/Diagnosis.java
+++ src/main/java/ru/job4j/profession/Diagnosis.java
@@ -1,4 +1,4 @@
-package ru.job4j.inheritance;
+package ru.job4j.profession;
 
 public class Diagnosis extends Doctor {
     private String diagnostic;
