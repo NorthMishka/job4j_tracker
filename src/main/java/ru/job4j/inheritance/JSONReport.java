124ef008805df611b43e172641e61cfa1d65dd61
John_Smith <the.tylertyler@gmail.com>
2021-02-26 00:07:36 +0300

    4. Переопределение [#459531]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,8 +3,13 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport{
     @Override
     public String generate(String name, String body) {
-        char quote = '"';
-        return "{"+ System.lineSeparator() + "  " + quote + "name" + quote + " : " + quote + name + quote + "," + System.lineSeparator() + "  " + quote + "body" + quote + " : " + quote + body + quote + System.lineSeparator() + "}";
+        return "{"+
+                System.lineSeparator() +
+                "  " + "name" + " : " + name +
+                System.lineSeparator() +
+                "  " + "body" + " : " + body +
+                System.lineSeparator() +
+                "}";
     }
 }
 
