6d3ca09493bb9da4761733aafc946312441dd9e9
John_Smith <the.tylertyler@gmail.com>
2021-02-26 00:57:39 +0300

    7. Date. Отображение даты. [#459534]

--- src/main/java/ru/job4j/oop/Item.java
+++ src/main/java/ru/job4j/oop/Item.java
@@ -1,17 +1,24 @@
 package ru.job4j.oop;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private String name;
     private int id;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
     public Item(int id) {
+
         this.id = id;
     }
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public LocalDateTime getCreated() {
+        return created;
     }
     public static void main(String[] args) {
         Item item = new Item();
