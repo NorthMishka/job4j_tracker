58b9f11595d1113ef1b12681684759b9cafa58d2
John_Smith <the.tylertyler@gmail.com>
2021-02-27 01:43:55 +0300

    5. Tracker - хранилище [#459547]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,11 +7,9 @@ public class Item {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
