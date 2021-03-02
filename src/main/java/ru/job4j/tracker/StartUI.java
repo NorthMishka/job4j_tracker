6923ad4f4b9169c33abbd7afa91c0a5fce6b8608
John_Smith <the.tylertyler@gmail.com>
2021-03-03 00:40:44 +0300

    4.2. Статические методы. [#459474]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,85 +1,99 @@
 package ru.job4j.tracker;
 
 
-
 public class StartUI {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new task ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+    public static void findAllItem(Tracker tracker) {
+        System.out.println("=== Show all task ====");
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit task ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        String name = input.askStr("Enter new name: ");
+        Item item = new Item(name);
+        boolean result = tracker.replace(id, item);
+        if (result) {
+            System.out.println("Task id " + id + " edit");
+        } else {
+            System.out.println("Unknown task");
+        }
+    }
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Delete task ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        System.out.println(id);
+        boolean result = tracker.delete(id);
+        if (result) {
+            System.out.println("Task id " + id + " delete");
+        } else {
+            System.out.println("Unknown task");
+        }
+    }
+    public static void findByIdItem(Input input, Tracker tracker) {
+        System.out.println("=== Find task by Id ====");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        System.out.println(id);
+        Item result = tracker.findById(id);
+        if (result != null) {
+            System.out.println("Name: " + result.getName() + " id: " + result.getId());
+        } else {
+            System.out.println("Unknown task");
+        }
+    }
+    public static void findByNameItem(Input input, Tracker tracker) {
+        System.out.println("===  Find task by name ====");
+        String name = input.askStr("Enter name: ");
+        Item[] item = tracker.findByName(name);
+        if (item.length > 0) {
+            for (Item value : item) {
+                System.out.println(value);
+            }
+        } else {
+            System.out.println("Unknown task");
+        }
+    }
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int enter = Integer.parseInt(input.askStr("Select: "));
-            switch (enter){
+            switch (enter) {
                 case 0:
-                    System.out.println("Creating a new task");
-                    String name = input.askStr("Enter name: ");
-                    Item item = new Item(name);
-                    tracker.add(item);
+                    StartUI.createItem(input, tracker);
                     break;
                 case 1:
-                    System.out.println("All tasks:");
-                    Item[] items = tracker.findAll();
-                    for (int i = 0; i < items.length; i++) {
-                        System.out.println(items[i]);
-                    }
+                    StartUI.findAllItem(tracker);
                     break;
                 case 2:
-                    System.out.println("Edit task");
-                    int id = Integer.parseInt(input.askStr("Enter id: "));
-                    name = input.askStr("Enter new name task: ");
-                    item = new Item(name);
-                    boolean rls = tracker.replace(id, item);
-                    if (rls) {
-                        System.out.println("Task id " + id + " edit " + item);
-                    } else {
-                        System.out.println("Unknown task");
-                    }
+                    StartUI.replaceItem(input, tracker);
                     break;
                 case 3:
-                    System.out.println("Delete task");
-                    id = Integer.parseInt(input.askStr("Enter id: "));
-                    System.out.println(id);
-                    rls = tracker.delete(id);
-                    if (rls) {
-                        System.out.println("Task id " + id + " delete");
-                    } else {
-                        System.out.println("Unknown task");
-                    }
+                    StartUI.deleteItem(input, tracker);
                     break;
                 case 4:
-                    System.out.println("Find task by Id");
-                    id = Integer.parseInt(input.askStr("Enter id: "));
-                    System.out.println(id);
-                    item = tracker.findById(id);
-                    if (item != null) {
-                        System.out.println("Name " + item.getName() + " id " + item.getId());
-                    } else {
-                        System.out.println("Unknown task");
-                    }
+                    StartUI.findByIdItem(input, tracker);
                     break;
                 case 5:
-                    System.out.println("Find task by name");
-                    name = input.askStr("Enter name: ");
-                    items = tracker.findByName(name);
-                        if (items.length > 0) {
-                            for (Item value : items) {
-                                System.out.println(value);
-                            }
-                        } else {
-                            System.out.println("Unknown task");
-                        }
+                    StartUI.findByNameItem(input, tracker);
                     break;
                 case 6:
                     run = false;
                     break;
-
                 default:
-                    System.out.println("ДОПИСАТЬ!");
+                    System.out.println("Unknown request!");
                     break;
             }
-            /* if */
         }
     }
-
     private void showMenu() {
         System.out.println("Menu.");
         System.out.println("0. Add new Item");
@@ -89,10 +103,7 @@ public class StartUI {
         System.out.println("4. Find item by Id");
         System.out.println("5. Find items by name");
         System.out.println("6. Exit Program");
-        /* добавить остальные пункты меню. */
     }
-
-
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
