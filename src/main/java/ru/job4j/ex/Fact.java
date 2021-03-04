3f5ad10b609cb1bad021e9edda8983f47951ae93
John_Smith <the.tylertyler@gmail.com>
2021-03-05 00:39:26 +0300

    0.3. Тестирование исключений с junit [#459488]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class Fact {
     public static void main(String[] args) {
         Fact fact = new Fact();
-        fact.calc(-1);
+        System.out.println(fact.calc(1));
     }
     public int calc(int n) {
         if (n < 0) {
